b9b1f5baa7a68520dc8a89180ef3f606fe596503
simran <simranag951@gmail.com>
2024-01-01 17:16:44 +0530

    added todo frontend only

---
+++ React/todo/client/todo/src/components/TodoWrapper.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react"
+import TodoForm from "./TodoForm"
+import {v4 as uuidv4} from 'uuid';
+import Todo from "./Todo";
+import EditTodoForm from "./EditTodoForm";
+uuidv4();
+
+const TodoWrapper = () => {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = todo => {
+    setTodos([...todos, {id: uuidv4(), task:todo, completed : false, isEditing : false}])
+   // console.log(todos)
+  }
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  }
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  }
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="TodoWrapper">
+      <h1>Get Things Done !</h1>
+      <TodoForm addTodo={addTodo} />
+      {/* display todos */}
+      {todos.map((todo) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            key={todo.id}
+            task={todo}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+            toggleComplete={toggleComplete}
+          />
+        )
+      )}
+    </div>
+  );
+};
+
+export default TodoWrapper
